b8af272d4f5390d4d6976f733845250a167b67b6
AhmedFady775 <fadiahmed298@gmail.com>
2022-07-14 18:27:34 +0200

    Sample Commit

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -6,12 +6,29 @@ import Logo from "../Home/BG.jpg";
 // import Getstarted from "./Getstarted";
 import { Link } from "react-router-dom";
 import logo from "../Home/img-logo.png";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  { url: "images/1.jpg" },
+  { url: "images/2.jpg" },
+  { url: "images/3.jpg" },
+  { url: "images/4.jpg" },
+  { url: "images/5.jpg" },
+  { url: "images/6.jpg" },
+  { url: "images/7.jpg" },
+];
 
 function Home() {
   return (
     <div className="hero-container">
       <img className="IMG1" src={Logo} alt="PIC" />
-      <img src={logo} alt="logo" width="450px" />
+      <SimpleImageSlider
+        width={896}
+        height={504}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />
       <br />
       <h1>.لبدأ التسوق الرجاء الضغط علي الازرار بالاسفل</h1>
       <br />
